b833333581ee786a8aaf39ed74f2bb406d748f2a
Yuyuan <yuyuan@macbook>
2025-05-12 20:10:30 -0700

    exp pos v2

--- export_pos.py
+++ export_pos.py
@@ -2,29 +2,31 @@ import unreal
 import json
 import os
 
-# get contral pawn
-player_controller = unreal.EditorLevelLibrary.get_level_player_controller(0)
-pawn = player_controller.get_pawn()
+# get agent name
+agent_class_name = "ThirdPersonCharacter"
 
-loc = pawn.get_actor_location()
-rot = pawn.get_actor_rotation()
+# search all actor
+actors = unreal.EditorLevelLibrary.get_all_level_actors()
 
-pose = {
-    "position": {
-        "x": round(loc.x, 2),
-        "y": round(loc.y, 2),
-        "z": round(loc.z, 2)
-    },
-    "rotation": {
-        "yaw": round(rot.yaw, 2),
-        "pitch": round(rot.pitch, 2),
-        "roll": round(rot.roll, 2)
+# find agent in actor list
+pawn = next((a for a in actors if agent_class_name in a.get_class().get_name()), None)
+
+if pawn:
+    loc = pawn.get_actor_location()
+    rot = pawn.get_actor_rotation()
+
+    pose = {
+        "position": {"x": round(loc.x, 2), "y": round(loc.y, 2), "z": round(loc.z, 2)},
+        "rotation": {"yaw": round(rot.yaw, 2), "pitch": round(rot.pitch, 2), "roll": round(rot.roll, 2)}
     }
-}
 
-# output
-output_path = os.path.expanduser("~/Desktop/pose_step0.json")
-with open(output_path, "w") as f:
-    json.dump(pose, f, indent=2)
+    # output json
+    output_path = "C:/SimLLM-Nav/output_pos_file/pose_step0.json"
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-print(f"[✓] Pose saved to: {output_path}")
+    with open(output_path, "w") as f:
+        json.dump(pose, f, indent=2)
+
+    print(f"[✓] Pose exported to: {output_path}")
+else:
+    print("[✗] No matching actor found in scene.")
